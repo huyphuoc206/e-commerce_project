3b855758b43c1e47625287b9b00b62233bc226b6
Huy Phuoc <huyphuoc206@gmail.com>
2020-12-22 12:51:47 +0700

    update

--- EcoStoreWebsite/src/main/java/com/ecostore/dao/IGenericDAO.java
+++ EcoStoreWebsite/src/main/java/com/ecostore/dao/IGenericDAO.java
@@ -4,7 +4,7 @@ import com.ecostore.mapper.IRowMapper;
 
 import java.util.List;
 
-public interface IGenericDAO {
+public interface IGenericDAO<T> {
     <T> List<T> query(String sql, IRowMapper<T> rowMapper, Object... parameters); // co the co nhieu tham so
 
     void update(String sql, Object... parameters);
--- EcoStoreWebsite/src/main/java/com/ecostore/dao/impl/AbstractDAO.java
+++ EcoStoreWebsite/src/main/java/com/ecostore/dao/impl/AbstractDAO.java
@@ -8,7 +8,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AbstractDAO implements IGenericDAO {
+public class AbstractDAO<T> implements IGenericDAO<T> {
 
     @Override
     public <T> List<T> query(String sql, IRowMapper<T> rowMapper, Object... parameters) {
